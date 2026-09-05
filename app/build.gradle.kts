bbfe19dbb4421d3ffaaa8a424a3a199509009025
Aryan Prabhakar <prabhakararyan2007@gmail.com>
2026-09-05 17:23:47 +0530

    Fix Parse Error: Sign release APKs automatically for direct phone installation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,11 +29,12 @@ android {
 
     buildTypes {
         release {
+            signingConfig = signingConfigs.getByName("debug")
             isMinifyEnabled = true
             isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro"
+                "proguard-rules.pro",
             )
         }
     }
